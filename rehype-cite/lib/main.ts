57eee9ac4f9cf4330577a06ba3cb1a4d2b26eff9
Benjamin Bray <benrbray@gmail.com>
2024-07-29 22:37:37 +0900

    [rehype-cite] read bibtex data from vfile.data.extractedBibtex

--- rehype-cite/lib/main.ts
+++ rehype-cite/lib/main.ts
@@ -11,6 +11,7 @@ import type { CiteItem } from "@benrbray/mdast-util-cite";
 import { BibLatexParser } from "biblatex-csl-converter"
 import { EntryObject } from "./types";
 import { formatter, formatBibString } from "./formatter/default";
+import { CitationHole, visitCitations } from "./process/citationVisitor";
 
 ////////////////////////////////////////////////////////////////////////////////
 
@@ -27,22 +28,132 @@ type FormattedCitation = {
 ////////////////////////////////////////////////////////////////////////////////
 
 export type RehypeCiteOptions = { 
-  bibFiles: string[]
+  bibFiles: string[],
+  detectBibTex?: {
+    /**
+     * When `true`, `rehype-cite` will attempt to parse bibliographic
+     * data from any BibTeX code blocks found in the `hast` tree.
+     */
+    enabled: boolean,
+    /** When `true`, detected BibTeX code blocks will be removed. */
+    removeBibTex: boolean
+  }
+}
+
+type RehypeCiteConfig = {
+  bibFiles: string[],
+  detectBibTex: {
+    enabled: boolean,
+    removeBibTex: boolean
+  }
+}
+
+/**
+ * Convert the user-friendly `RehypeCiteOptions` into a more explicit config
+ * object.  The main difference is that any missing _options_ must be filled
+ * in with a default value to create a _config_.
+ *
+ * This distinction between _options_ and _config_ allows for the package API
+ * to change without breaking the implementation, and vice-versa.
+ */
+const optionsToConfig = (options: RehypeCiteOptions): RehypeCiteConfig => {
+  const detectBibTex = options.detectBibTex || { enabled: true, removeBibTex: true };
+
+  return {
+    bibFiles: options.bibFiles,
+    detectBibTex
+  };
 }
 
 export function rehypeCite(options: RehypeCiteOptions) {
 
-  // biblatex-csl-converter
-  let inputBib = options.bibFiles.join("\n\n");
-  let parser = new BibLatexParser(inputBib, {processUnexpected: true, processUnknown: true})
+  let config = optionsToConfig(options);
+  return runRehypeCite(config);
+
+}
+
+export default rehypeCite;
+
+////////////////////////////////////////////////////////////////////////////////
+
+declare module 'vfile' {
+  interface DataMap {
+    extractedBibtex?: string[]
+  }
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+/**
+ * Uses `biblatex-csl-converter` to parse
+ * BibTeX strings into structured JSON.
+ */
+const parseBibtex = (bibtex: string[]): EntryObject[] => {
+  if(bibtex.length === 0) { return []; }
+
+  let input = bibtex.join("\n\n");
+  let parser = new BibLatexParser(input, { processUnexpected: true, processUnknown: true });
   let bibJson = parser.parse();
-  let bibEntries = Object.values(bibJson.entries) as EntryObject[];
+  return Object.values(bibJson.entries) as EntryObject[];
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+const getEntry = (entries: EntryObject[], key: string): EntryObject | null => {
+  return entries.find(entry => entry.entry_key === key) || null;
+}
+
+/**
+ * Replaces the placeholder citation from `remark-cite`
+ * with a fully-rendered inline citation.
+ */
+const processInlineCite = (
+  formatted: Partial<Record<string, FormattedCitation>>,
+  citeItems: CiteItem[],
+  element: HastElement
+) => {
+  const entryIds: ElementContent[] = citeItems.map(ci => {
+    const id = formatted[ci.key];
+    if(id) { return id.citeId; }
+    return { type: "text", value: `${ci.key}` };
+  });
+
+  element.children = [
+    { type: "text", value: "["},
+    ...entryIds.flatMap((e,i): ElementContent[] => {
+      if(i === 0) { return [e]; }
+      return [{ type: "text", value: ", " }, e];
+    }),
+    { type: "text", value: "]"},
+  ];
+}
+
+/**
+ * Replaces the placeholder citation from `remark-cite`
+ * with a fully-rendered block citation.
+ */
+const processBlockCite = (
+  formatted: Partial<Record<string, FormattedCitation>>,
+  citeItems: CiteItem[],
+  parentElement: HastElement,
+) => {
+  const entryIds: ElementContent[] = citeItems.map(ci => {
+    const id = formatted[ci.key];
+    if(id) { return id.citeBib; }
+    return { type: "text", value: `${ci.key}` };
+  });
+
+  parentElement.type = "element";
+  parentElement.tagName = "div";
+  parentElement.properties.className = ["cite-block"];
+  parentElement.children = entryIds;
+};
+
+const runRehypeCite = (config: RehypeCiteConfig) => {
+
+  let baseBibEntries = parseBibtex(config.bibFiles);
 
   /* ---------------------------------------------------- */
-
-  const getEntry = (key: string): EntryObject | null => {
-    return bibEntries.find(entry => entry.entry_key === key) || null;
-  }
 
   const processBibliography = (
     formatted: Partial<Record<string, FormattedCitation>>,
@@ -53,12 +164,12 @@ export function rehypeCite(options: RehypeCiteOptions) {
       return {
         type: "element",
         tagName: "div",
-        properties: { className: "bib-entry" },
+        properties: { className: ["bib-entry"] },
         children: [
           {
             type: "element",
             tagName: "div",
-            properties: { className: "bib-id" },
+            properties: { className: ["bib-id"] },
             children: [
               { type: "text", value: "[" },
               fmt!.citeId,
@@ -73,67 +184,30 @@ export function rehypeCite(options: RehypeCiteOptions) {
     const biblioTitle: HastElement = {
       type: "element",
       tagName: "div",
-      properties: { "className" : "bib-title" },
+      properties: { "className" : ["bib-title"] },
       children: [{ type: "text", value: "Bibliography" }]
     };
 
     return {
       type: "element",
       tagName: "div",
-      properties: { className: "rehype-cite-bib" },
+      properties: { className: ["rehype-cite-bib"] },
       children: [biblioTitle, {
         type: "element",
         tagName: "div",
-        properties: { className: "bib-entry-list" },
+        properties: { className: ["bib-entry-list"] },
         children: formattedEntries
     }]
     }
   }
 
-  // replaces the placeholder inline citation produced by remark-cite
-  // with a nicely-formatted html produced by citeproc-js
-  const processInlineCite = (
-    formatted: Partial<Record<string, FormattedCitation>>,
-    citeItems: CiteItem[],
-    element: HastElement
-  ) => {
-    const entryIds: ElementContent[] = citeItems.map(ci => {
-      const id = formatted[ci.key];
-      if(id) { return id.citeId; }
-      return { type: "text", value: `${ci.key}` };
-    });
-
-    element.children = [
-      { type: "text", value: "["},
-      ...entryIds.flatMap((e,i): ElementContent[] => {
-        if(i === 0) { return [e]; }
-        return [{ type: "text", value: ", " }, e];
-      }),
-      { type: "text", value: "]"},
-    ];
-  }
-
-  const processBlockCite = (
-    formatted: Partial<Record<string, FormattedCitation>>,
-    citeItems: CiteItem[],
-    parentElement: HastElement,
-  ) => {
-    const entryIds: ElementContent[] = citeItems.map(ci => {
-      const id = formatted[ci.key];
-      if(id) { return id.citeBib; }
-      return { type: "text", value: `${ci.key}` };
-    });
-
-    parentElement.type = "element";
-    parentElement.tagName = "div";
-    parentElement.properties.className = "cite-block";
-    parentElement.children = entryIds;
-  };
-
   /**
    * Renders inline citations and bibliography entries for the given keys.
    */
-  const formatCitations = (citeKeys: string[]): Partial<Record<string, FormattedCitation>> => {
+  const formatCitations = (
+    bibEntries: EntryObject[],
+    citeKeys: string[]
+  ): Partial<Record<string, FormattedCitation>> => {
     const result: Partial<Record<string, FormattedCitation>> = {};
 
     citeKeys.forEach((key, idx) => {
@@ -141,18 +215,18 @@ export function rehypeCite(options: RehypeCiteOptions) {
       if(key in result) { return; }
 
       // handle missing entries
-      const citeEntry = getEntry(key);
+      const citeEntry = getEntry(bibEntries, key);
       if(!citeEntry) {
         const citeId: HastElement = {
           type: "element",
           tagName: "span",
-          properties: { className: "bib-id" },
+          properties: { className: ["bib-id"] },
           children: [{ type: "text", value: `${idx}` }]
         };
         const citeBib: HastElement = {
           type: "element",
           tagName: "div",
-          properties: { className: "bib-error" },
+          properties: { className: ["bib-error"] },
           children: [{ type: "text", value: `${key}` }]
         };
 
@@ -172,7 +246,7 @@ export function rehypeCite(options: RehypeCiteOptions) {
         type: "element",
         tagName: "span",
         properties: {
-          className: "bib-id",
+          className: ["bib-id"],
           title: citeBibString
         },
         children: [{ type: "text", value: `${idx}` }]
@@ -192,40 +266,34 @@ export function rehypeCite(options: RehypeCiteOptions) {
 
   /* ---- transform ------------------------------------- */
 
-  return function(tree: Root, _file: VFile): undefined {
+  return function(tree: Root, file: VFile): undefined {
+
+    // check the `VFile.data.extractedBibtex` field for extra reference entries
+    // which should be used when rendering citations for this document
+    // (this allows other plugins to influence the behavior of `rehype-cite`)
+    const extractedBibtex: string[] = file.data.extractedBibtex || [];
+
+    // traverse the hast syntax tree, collecting all cite-inline nodes
     let citations: CiteItem[] = [];
-    let citationsInline: { element: HastElement, isBlock: boolean, citeItems: CiteItem[] }[] = [];
-
-    // traverse the hast syntax tree and collect all cite-inline nodes
-    visit(tree, 'element', function(element, _index, _parent): VisitorResult {
-      // look for elements marked with "cite-inline" class
-      const classes = Array.isArray(element.properties.className)
-        ? element.properties.className
-        : [];
-      const isCiteInline = classes.includes("cite-inline");
-      if(!isCiteInline) { return CONTINUE; }
-      
-      // validate data-cite attribute
-      const citeData = element.properties["dataCite"];
-      if(!citeData) { return CONTINUE; }
-      if(typeof citeData !== "string") { return CONTINUE; }
-
-      const citeItems = JSON.parse(citeData) as CiteItem[];
-      citations = citations.concat(citeItems);
-
-      const isBlock = (!!_parent && _parent.type === "element" && _parent.tagName === "p" && _parent.children.length === 1);
-      
-      if(isBlock) {
-        citationsInline.push({ element: _parent as HastElement, citeItems, isBlock });
-      } else {
-        citationsInline.push({ element, citeItems, isBlock });
+    let citationsInline: CitationHole[] = [];
+    visit(tree, 'element', (element, _index, parent): VisitorResult => {
+      // look for citation `hast` nodes produced by `remark-cite`
+      const citeHole = visitCitations(element, parent as HastElement);
+      if(citeHole !== null) {
+        citations = citations.concat(citeHole.citeItems);
+        citationsInline.push(citeHole);
       }
 
-      return SKIP;
+      if(citeHole === null) {
+        return CONTINUE;
+      } else {
+        return SKIP;
+      }
     });
 
     // generate inline citations and bibliography entries for all mentioned keys
-    let formatted = formatCitations(citations.map(c => c.key));
+    let extractedBibEntries = parseBibtex(extractedBibtex);
+    let formatted = formatCitations(baseBibEntries.concat(extractedBibEntries), citations.map(c => c.key));
 
     // insert inline citations into the hast tree
     citationsInline.forEach(({ element, isBlock, citeItems }) => {
@@ -242,5 +310,3 @@ export function rehypeCite(options: RehypeCiteOptions) {
     }
   }
 }
-
-export default rehypeCite;
