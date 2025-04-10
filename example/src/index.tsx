16949dadb9bfa98e39b82ba1b2eb31ffddd967c4
Benjamin Bray <benrbray@gmail.com>
2025-04-10 15:01:36 +0900

    import css in example

--- example/src/index.tsx
+++ example/src/index.tsx
@@ -3,6 +3,7 @@ import { render } from 'solid-js/web';
 
 import './index.css';
 
+import '@benrbray/rehype-cite/dist/rehype-cite.css'
 import "katex/dist/katex.min.css";
 import { Demo } from "./Demo";
 
